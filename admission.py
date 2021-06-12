c8fe15a6f1f44265ab5df5c51cc0a28804471b34
mes_murugan <mes.murugan@gmail.com>
2021-06-12 14:56:31 +0530

    Updated

---
+++ admission.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import pickle
+st.write("""
+# SVM implementation app
+ """)
+svm_classifier = open('svm_model.pkl','rb')
+classifier = pickle.load(svm_classifier)
+#Text Input
+gre = st.text_input("Enter the GRE Score:")
+toefl = st.text_input("Enter the TOEFL Score:")
+ur =  st.text_input("Enter the University Rating:")
+sop =  st.text_input("Enter the SOP:")
+lor =  st.text_input("Enter the LOR:")
+cgpa =  st.text_input("Enter the CGPA:")
+research =  st.text_input("Enter the Research:")
+submit = st.button("Check")
+
+if submit:
+        result = classifier.predict([[gre,toefl,ur,sop,lor,cgpa,reserach]])
+        if result ==1:
+                st.write("Will Get Admission")
+        else:
+                st.write("No Admission")                
+                #st.write(result)
